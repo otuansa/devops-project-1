e9e1e10f393494ed91259842cad9003322debce2
otuansa <ansaotuekong@gmail.com>
2025-06-02 23:13:24 +0100

    update

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -9,7 +9,7 @@ variable "user_data_install_apache" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /Users/macbook/downloads/mykeypair1 ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+  value = format("%s%s", "ssh -i /home/ubuntu/keys/mykeypair1 ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
 }
 
 output "dev_proj_1_ec2_instance_id" {
@@ -35,7 +35,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   }
 }
 
-/*resource "aws_key_pair" "dev_proj_1_public_key" {
-  key_name   = "aws_key"
+resource "aws_key_pair" "dev_proj_1_public_key" {
+  key_name   = "mykeypair1"
   public_key = var.public_key
-}*/
+}

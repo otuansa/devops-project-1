785bd13f2cd2865f6b02e5e4d343a1aaecc3cd0e
otuansa <ansaotuekong@gmail.com>
2025-06-03 03:23:35 +0100

    Update

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -9,7 +9,7 @@ variable "user_data_install_apache" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/ubuntu/keys/mykeypair1 ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+  value = format("%s%s", "ssh -i /home/ubuntu/keys/Jenkins ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
 }
 
 output "dev_proj_1_ec2_instance_id" {
@@ -19,11 +19,10 @@ output "dev_proj_1_ec2_instance_id" {
 resource "aws_instance" "dev_proj_1_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name = aws_key_pair.dev_proj_key.key_name
   tags = {
     Name = var.tag_name
   }
-  key_name                    = "mykeypair1"
+  key_name                    = "Jenkins"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
   associate_public_ip_address = var.enable_public_ip_address
@@ -36,19 +35,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   }
 }
 
-# resource "aws_key_pair" "dev_proj_1_public_key" {
-#   key_name   = "mykeypair1"
-#   public_key = var.public_key
-# }
-
-# Option A: If you have the key pair file locally but need to create it in AWS
-resource "aws_key_pair" "dev_proj_key" {
-  key_name   = "mykeypair1"
-  public_key = file("~/.ssh/id_rsa.pub") # Or path to your downloaded .pub file
-}
-
-# # Then reference it in your EC2 instance:
-# resource "aws_instance" "dev_proj_1_ec2" {
-#   key_name = aws_key_pair.dev_proj_key.key_name
-#   # ... rest of config ...
-# }
+/*resource "aws_key_pair" "dev_proj_1_public_key" {
+  key_name   = "aws_key"
+  public_key = var.public_key
+}*/
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -8,7 +8,7 @@ cidr_public_subnet   = ["10.0.1.0/24", "10.0.2.0/24"]
 cidr_private_subnet  = ["10.0.3.0/24", "10.0.4.0/24"]
 eu_availability_zone = ["eu-central-1a", "eu-central-1b"]
 
-public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCvLRaX/+PEDv3zaq3+SfJBNcv8JH5gqniUzhDTJis++Bu0C27pcCVwC687nJoiptr6IM5zZ+3tRjSgr0p3crkeLQb3RWNhVPsZFpySAXIEGcvTDbLk7bhv+jtBTJ5/F3nTWXdw+XG2abWscuAgA0E4lhcApWtk6QX7ICJlmC70SaruLa6boz/YTLRnK+ib2xC3qu1k/5BcmkHD0E22Y5kBFumFfz5QCiFP5I7tJodJdOEcjU0tPNFB6Xzy2JJQw9k9pENEGx7mOzg3KAlV4CAuuC5GhKINPZT7/nDb0AFqt15Go2yldR2J4isO9eDeEYaqrEHedcoG6r2wLV5d7fP" #Replace with your public key
+public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2Dgoq0tJf6WUBVOQv17/5U/AaxSaULVBmAjuIW9OzwR9NQWhTgqE+N7RYTRRcAQf6px5bWS093FaU9SjAftz3DfJzi9YgqpqvxWlhRwVKUPuFXNGjjwIFMJCBKXfwLOnNTnwAifmzFt9xZICB834C9KpYNwcxAbLASTx/atnw091/V78hK8UY7yyVjA3ikowNG8Px6jhMWs6ApKCjzLhJrtZM+zfkHejTgBU7b+w0hjWu8/YYMt5gBw9FISWW47Q7m5Qhz2VJHiDDMudacpnRBhTQ5ur49rbSBj36Ze8CJuQzloBE+kkt0D4NCkP5npP+HO7r5UxRuXBlOzIIvPef" #Replace with your public key
 ec2_ami_id     = "ami-03250b0e01c28d196"
 
 ec2_user_data_install_apache = ""

a16e4c8f91d87c58ce233735afa99893cb34a86a
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-10 00:06:46 +0100

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
   associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_apache
+  user_data = var.ec2_user_data_install_apache
 
   metadata_options {
     http_endpoint = "enabled"  # Enable the IMDSv2 endpoint

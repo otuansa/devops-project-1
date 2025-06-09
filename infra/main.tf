200db9736c84f1d024e20db2e5fd218269bd38c2
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-10 00:50:01 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -22,10 +22,6 @@ module "security_group" {
   ec2_sg_name_for_python_api = "SG for EC2 for enabling port 5000"
 }
 
-locals {
-  ec2_user_data_install_apache = templatefile("./template/ec2_install_apache.sh", {})
-}
-
 module "ec2" {
   source                   = "./ec2"
   ami_id                   = var.ec2_ami_id
@@ -37,8 +33,6 @@ module "ec2" {
   ec2_sg_name_for_python_api     = module.security_group.sg_ec2_for_python_api
   enable_public_ip_address = true
   user_data_install_apache = templatefile("./template/ec2_install_apache.sh", {})
-  
-
 }
 
 module "lb_target_group" {

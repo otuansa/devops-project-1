3e6e0672f482d425bb1ab60e54430cc1501e5709
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-09 03:03:30 +0100

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,19 +1,12 @@
-
-#!/bin/bash
+#! /bin/bash
+# shellcheck disable=SC2164
 cd /home/ubuntu
-
-# Install required packages
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
-
-# Clone the repo
 git clone https://github.com/otuansa/python-mysql-db-proj-1.git
 sleep 20
+# shellcheck disable=SC2164
 cd python-mysql-db-proj-1
-
-# Ensure setup script is executable and run it
-chmod +x setup-env.sh
-./setup-env.sh
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &

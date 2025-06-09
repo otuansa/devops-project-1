b23f8a060403cf2f2a6ae370b3ede979f1b5e690
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-09 02:29:06 +0100

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -7,7 +7,7 @@ yes | sudo apt update
 yes | sudo apt install -y python3 python3-pip python3.12-venv git
 
 # Clone the repo
-https://github.com/otuansa/python-mysql-db-proj-1.git
+git clone https://github.com/otuansa/python-mysql-db-proj-1.git
 sleep 20
 cd python-mysql-db-proj-1
 

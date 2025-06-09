2e756dfcdf3c9ed96201888dfc8cf897aaf7685e
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-10 00:12:06 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -33,7 +33,6 @@ module "ec2" {
   ec2_sg_name_for_python_api     = module.security_group.sg_ec2_for_python_api
   enable_public_ip_address = true
   user_data_install_apache = templatefile("./template/ec2_install_apache.sh", {})
-  user_data = user_data_install_apache
 }
 
 module "lb_target_group" {

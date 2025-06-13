c8371ed6cdf650cfa9190decb3fde72829182c58
OtuEkong Ansa <54278721+otuansa@users.noreply.github.com>
2025-06-13 16:13:15 +0100

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -5,11 +5,11 @@ variable "public_key" {}
 variable "subnet_id" {}
 variable "sg_enable_ssh_https" {}
 variable "enable_public_ip_address" {}
-variable "user_data_install_apache" {}
+variable "user_data_install_flaskapp" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/ubuntu/keys/Jenkins ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+  value = format("%s%s", "ssh -i '/Users/Macbook/Downloads/Jenkins.pem' ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
 }
 
 output "dev_proj_1_ec2_instance_id" {
@@ -27,7 +27,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
   associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_apache
+  user_data = var.user_data_install_flaskapp
 
   metadata_options {
     http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
@@ -35,7 +35,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   }
 }
 
-/*resource "aws_key_pair" "dev_proj_1_public_key" {
-  key_name   = "aws_key"
-  public_key = var.public_key
-}*/
+# resource "aws_key_pair" "dev_proj_1_public_key" {
+#  key_name   = "aws_key"
+#  public_key = var.public_key
+# }
